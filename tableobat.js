4b46577af56c2ef55eab8c4133040283152fe6ea
royantristan <royantris@gmail.com>
2021-08-27 09:41:16 +0700

    done

---
+++ tableobat.js
@@ -0,0 +1,108 @@
+const table_obat = [
+  {
+    id_obat: D1,
+    nama_obat: "panadol",
+    tipe: "tablet",
+    jenis_obat: "demam",
+    stok: 4,
+    harga: 5000,
+    
+  },
+  {
+    id_obat: D2,
+    nama_obat: "sanmol",
+    tipe: "cair",
+    jenis_obat: "demam",
+    stok: 8,
+    harga: 7000,
+    
+  },
+  {
+    id_obat: D3,
+    nama_obat: "paracetamol",
+    tipe: "tablet",
+    jenis_obat: "demam",
+    stok: 10,
+    harga: 10000,
+    
+  },
+  {
+    id_obat: F1,
+    nama_obat: "mixagrip",
+    tipe: "tablet",
+    jenis_obat: "flu",
+    stok: 11,
+    harga: 6000,
+    
+  },
+  {
+    id_obat: F2,
+    nama_obat: "decolgen",
+    tipe: "tablet",
+    jenis_obat: "flu",
+    stok: 9,
+    harga: 6000,
+    
+  },
+  {
+    id_obat: F3,
+    nama_obat: "triaminic",
+    tipe: "cair",
+    jenis_obat: "flu",
+    stok: 12,
+    harga: 8000,
+    
+  },
+  {
+    id_obat: B1,
+    nama_obat: "woods",
+    tipe: "cair",
+    jenis_obat: "batuk",
+    stok: 10,
+    harga: 7000,
+    
+  },
+  {
+    id_obat: B2,
+    nama_obat: "OBH combi",
+    tipe: "cair",
+    jenis_obat: "batuk",
+    stok: 15,
+    harga: 9000,
+    
+  },
+
+  {
+    id_obat: B3,
+    nama_obat: "konidin",
+    tipe: "tablet",
+    stok: 14,
+    harga: 4000,
+    
+  },
+  {
+    id_obat: S1,
+    nama_obat: "vitalong c",
+    tipe: "kapsul",
+    stok: 10,
+    harga: 12000,
+    
+  },
+  {
+    id_obat: S2,
+    nama_obat: "imboost",
+    tipe: "tablet",
+    stok: 11,
+    harga: 15000,
+    
+  },
+  {
+    id_obat: S3,
+    nama_obat: "stimuno",
+    tipe: "kapsul",
+    stok: 12,
+    harga: 2000,
+    
+  },
+ ];
+
