658e6bb683d162c3575f7db6953d5874942ea1ec
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-08-27 10:29:29 +0700

    user 40%

--- assets/js/apps/listObat.js
+++ assets/js/apps/listObat.js
@@ -1,16 +1,137 @@
+const table_obat = [
+  {
+    id_obat: "D1",
+    nama_obat: "panadol",
+    tipe: "tablet",
+    jenis_obat: "demam",
+    stok: 4,
+    harga: 5000,
+    src: "panadol.jfif",
+  },
+  {
+    id_obat: "D2",
+    nama_obat: "sanmol",
+    tipe: "cair",
+    jenis_obat: "demam",
+    stok: 8,
+    harga: 7000,
+    src: "sanmol.jfif",
+  },
+  {
+    id_obat: "D3",
+    nama_obat: "paracetamol",
+    tipe: "tablet",
+    jenis_obat: "demam",
+    stok: 10,
+    harga: 10000,
+    src: "paracetamol.jfif",
+  },
+  {
+    id_obat: "F1",
+    nama_obat: "mixagrip",
+    tipe: "tablet",
+    jenis_obat: "flu",
+    stok: 11,
+    harga: 6000,
+    src: "mixagrip.jfif",
+  },
+  {
+    id_obat: "F2",
+    nama_obat: "decolgen",
+    tipe: "tablet",
+    jenis_obat: "flu",
+    stok: 9,
+    harga: 6000,
+    src: "decolgen.jfif",
+  },
+  {
+    id_obat: "F3",
+    nama_obat: "triaminic",
+    tipe: "cair",
+    jenis_obat: "flu",
+    stok: 12,
+    harga: 8000,
+    src: "triaminic.jfif",
+  },
+  {
+    id_obat: "B1",
+    nama_obat: "woods",
+    tipe: "cair",
+    jenis_obat: "batuk",
+    stok: 10,
+    harga: 7000,
+    src: "woods.jfif",
+  },
+  {
+    id_obat: "B2",
+    nama_obat: "OBH combi",
+    tipe: "cair",
+    jenis_obat: "batuk",
+    stok: 15,
+    harga: 9000,
+    src: "obhCombi.jfif",
+  },
+
+  {
+    id_obat: "B3",
+    nama_obat: "konidin",
+    tipe: "tablet",
+    jenis_obat: "batuk",
+    stok: 14,
+    harga: 4000,
+    src: "konidin.jfif",
+  },
+  {
+    id_obat: "S1",
+    nama_obat: "vitalong c",
+    tipe: "kapsul",
+    jenis_obat: "suplemen",
+    stok: 10,
+    harga: 12000,
+    src: "vitalongC.jfif",
+  },
+  {
+    id_obat: "S2",
+    nama_obat: "imboost",
+    tipe: "tablet",
+    jenis_obat: "suplemen",
+    stok: 11,
+    harga: 15000,
+    src: "imboost.jfif",
+  },
+  {
+    id_obat: "S3",
+    nama_obat: "stimuno",
+    tipe: "kapsul",
+    jenis_obat: "suplemen",
+    stok: 12,
+    harga: 2000,
+    src: "stimuno.jfif",
+  },
+];
+
 let cardObat = document.getElementById("cardObat");
 let output = "";
-let tabelObat = JSON.parse(localStorage.getItem("tabelObat"));
+let tabelObat = [];
 
 let formatter = new Intl.NumberFormat("en-us", {
   style: "currency",
   currency: "IDR",
 });
 
-console.log(formatter.format(300000));
 getStok();
 
 function getStok() {
+  if (localStorage.getItem("tabelObat")) {
+    tabelObat = JSON.parse(localStorage.getItem("tabelObat"));
+  } else {
+    for (const obat of table_obat) {
+      tabelObat.push(obat);
+    }
+
+    localStorage.setItem("tabelObat", JSON.stringify(tabelObat));
+  }
+
   for (const obat of tabelObat) {
     output += `
     <div class="card m-3" style="width: 18rem;">
